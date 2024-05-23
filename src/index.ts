ccc3ac6905571789e8fafb2cddac3e11ee46fc35
hirotask <hirotaskhashi@gmail.com>
2024-05-23 00:23:00 +0000

    add testing library

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import users from './controller/users'
+
 const app: express.Express = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -15,19 +17,4 @@ app.listen(3000, () => {
     console.log("Start on port 3000.")
 })
 
-type User = {
-    id: number
-    name: string
-    email: string
-};
-
-const users: User[] = [
-    { id: 1, name: "User1", email: "user1@test.local" },
-    { id: 2, name: "User2", email: "user2@test.local" },
-    { id: 3, name: "User3", email: "user3@test.local" }
-]
-
-//一覧取得
-app.get('/users', (req: express.Request, res: express.Response) => {
-    res.send(JSON.stringify(users))
-})
+app.use("/users", users)
